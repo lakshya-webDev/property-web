56513ffcf6dd34c6408bbaf4b4506bff4847bceb
Lakshya Choudhary <lakshya.choudhary-c@jumeirah.com>
2023-10-30 18:50:04 +0530

    Change issue

--- src/components/HeroBanner/index.js
+++ src/components/HeroBanner/index.js
@@ -1,26 +1,21 @@
 "use client"
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import HeroMap from "../HeroMap";
-import Loader from "../Loader";
 import useCurrentLocation from "@/hooks/useCurrentLocation";
 import { useSelector } from "react-redux";
 const HeroBanner = () => {
   const {location}= useSelector((state)=>state.currentLocation);
+  const [defaultLocation, setDefaultLocation] = useState(location ? location : {latitude:25.141579399999998 ,longitude:55.19085808643323})
+  console.log(defaultLocation,"Location");
+  
   useCurrentLocation();
   return (
     <section className="mb-10 relative h-96 w-full bg-slate-400">
       <div className="bg-cover bg-center bg-no-repeat h-full relative z-0">
         <HeroMap
-          latitude={location && location.latitude}
-          longitude={location && location.longitude}
+          latitude={defaultLocation && defaultLocation.latitude}
+          longitude={defaultLocation && defaultLocation.longitude}
         />
-        {/*
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center w-full max-w-[800px] px-6">
-          <h2 className="mb-6 text-2xl font-semibold leading-tight tracking-tight md:text-3xl xl:text-6xl capitalize">
-          Properties in UAE
-          </h2>
-        </div>  
-      */}
       </div>
     </section>
   );
